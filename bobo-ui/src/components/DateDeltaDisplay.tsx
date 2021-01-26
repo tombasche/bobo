0440aa1298dbddf963f9707b44d2c1952a8652e9
Tom Basche <tcbasche@gmail.com>
2021-01-26 20:16:30 +1100

    Remove unused import

--- bobo-ui/src/components/DateDeltaDisplay.tsx
+++ bobo-ui/src/components/DateDeltaDisplay.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import styled from 'styled-components';
 import { diffDate } from '../dateUtils/diff';
 import { parseServerDate } from '../dateUtils/parse';
 import { SmallFont } from './SmallFont';
