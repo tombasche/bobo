6a91a0949afea7ae45f2056efb2671bc1b4076a8
Tom Basche <tcbasche@gmail.com>
2021-01-26 14:29:43 +1100

    Fix typo in test name

--- bobo-ui/src/components/Rating.test.tsx
+++ bobo-ui/src/components/Rating.test.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
 import { toEmoji } from './Rating';
 
-test('Convert a number into graphical representation of a rating', () => {
+test('Convert a number into a graphical representation of a rating', () => {
     const result = toEmoji(1.0)
     expect(result).toEqual("😠");
 });
