cf7f3b62a1e5a125076dc0daeb7c06ca4d7c64a6
Tom Basche <tcbasche@gmail.com>
2021-02-13 17:46:58 +1100

    Formatting fixes

--- bobo-ui/src/backend/book/validate.ts
+++ bobo-ui/src/backend/book/validate.ts
@@ -14,21 +14,21 @@ export const validate = (b: NewBook): Result<NewBook, ValidFields> => {
     const message = validateBook(b)
     if (!isValid(message)) return {ok: false, message}
     return  {ok: true, value: b};
-}
-
-type KeyableNewBook = NewBook & {[key: string]: string | number | string[]}
-
-const hasField = (b: KeyableNewBook, field: string): boolean => {
-    return b.hasOwnProperty(field) && !!(b[field]);
-} 
+};
 
 const validateBook = (b: NewBook): ValidFields => {
     return RequiredBookFields.reduce((obj: ValidFields, e: string) => {
         obj[e] = hasField(b, e);
         return obj;
     }, {});
-}
+};
+
+type KeyableNewBook = NewBook & {[key: string]: string | number | string[]};
+
+const hasField = (b: KeyableNewBook, field: string): boolean => {
+    return b.hasOwnProperty(field) && !!(b[field]);
+};
 
 const isValid = (f: ValidFields): boolean => {
     return Object.values(f).every((b) => b)
-}
+};
