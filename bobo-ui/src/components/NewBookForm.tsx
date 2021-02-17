43e5781ca97d35384b0a0bb4715e314abae5b398
Tom Basche <tcbasche@gmail.com>
2021-02-17 19:33:47 +1100

    Wrap all fields in a div

--- bobo-ui/src/components/NewBookForm.tsx
+++ bobo-ui/src/components/NewBookForm.tsx
@@ -10,12 +10,17 @@ const BookForm = styled.form`
   display: flex;
   flex-flow: column;
   align-items: flex-start;
-  width: 100%;
 `;
 
 const Field = styled.div`
   display: flex;
   align-items: center;
+`;
+
+const Fields = styled.div`
+  display: flex;
+  align-items: flex-start;
+  flex-flow: column;
 `;
 const Label = styled.label`
   padding-right: 5px;
@@ -30,31 +35,33 @@ interface NewBookProps {
 const NewBookForm = ({ book, submit, change }: NewBookProps) => {
   return (
     <BookForm onSubmit={(e) => submit(e, book)}>
-      <Field>
-        <Label>Title</Label>
-        <BookInputField name="title" value={book.title} change={change} />
-      </Field>
-      <Field>
-        <Label>Author</Label>
-        <BookInputField name="author" value={book.author} change={change} />
-      </Field>
-      <Field>
-        <Label>Rating</Label>
-        <SelectRating
-          name="rating"
-          value={book.rating.toString()}
-          change={change}
-        />
-      </Field>
-      <Field>
-        <Label>Genres</Label>
-        <BookMultiSelectField
-          name="genres"
-          values={book.genres}
-          options={['Fantasy', 'Action', 'Non-fiction']}
-          change={change}
-        />
-      </Field>
+      <Fields>
+        <Field>
+          <Label>Title</Label>
+          <BookInputField name="title" value={book.title} change={change} />
+        </Field>
+        <Field>
+          <Label>Author</Label>
+          <BookInputField name="author" value={book.author} change={change} />
+        </Field>
+        <Field>
+          <Label>Rating</Label>
+          <SelectRating
+            name="rating"
+            value={book.rating.toString()}
+            change={change}
+          />
+        </Field>
+        <Field>
+          <Label>Genres</Label>
+          <BookMultiSelectField
+            name="genres"
+            values={book.genres}
+            options={['Fantasy', 'Action', 'Non-fiction']}
+            change={change}
+          />
+        </Field>
+      </Fields>
       <SubmitButton />
     </BookForm>
   );
