98de3e834148914d956a8bd6ba2c8c447d0c8b01
Tom Basche <tcbasche@gmail.com>
2021-02-23 10:50:02 +1100

    Add delay to mock book creation

--- bobo-ui/src/mocks/api/mutations.js
+++ bobo-ui/src/mocks/api/mutations.js
@@ -3,8 +3,8 @@ import { graphql } from 'msw';
 export const CREATE_BOOK_MUTATION = graphql.mutation(
   'createBook',
   (_, res, ctx) => {
-    // const { title, author, genres, rating, dateFinished } = req.variables;
     return res(
+      ctx.delay(),
       ctx.data({
         data: {
           id: 99,
