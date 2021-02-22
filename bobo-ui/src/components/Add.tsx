2ec403d708afff0cd3bfc6808ff03395003e9dd5
Tom Basche <tcbasche@gmail.com>
2021-02-23 10:29:38 +1100

    Move card components into a card dir

--- bobo-ui/src/components/Add.tsx
+++ bobo-ui/src/components/Add.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import BaseCardDiv from './BaseCard';
+import BaseCardDiv from './card/BaseCard';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
