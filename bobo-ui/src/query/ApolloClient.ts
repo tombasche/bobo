0ce87cf12741a5e2ee3fe70fe62f64158ed0ed8f
Tom Basche <tcbasche@gmail.com>
2021-02-25 17:00:11 +1100

    Merge cache updates correctly
    This just clobbers existing values with the new server response,
    which is fine because it's pretty low effort to just do this rather
    than have complex logic that splices in a new book, or removes the
    deleted one.

--- bobo-ui/src/query/ApolloClient.ts
+++ bobo-ui/src/query/ApolloClient.ts
@@ -1,6 +1,18 @@
 import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
 
-const cache = new InMemoryCache();
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        allBooks: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+      },
+    },
+  },
+});
 
 const link = new HttpLink({
   uri: process.env.REACT_APP_BACKEND,
